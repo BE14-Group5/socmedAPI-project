ff7edc7d8e94644923a3e736fcf0fd81c600f7f2
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-15 23:16:41 +0700

    feat: add service for register user

--- features/user/services/service.go
+++ features/user/services/service.go
@@ -1,9 +1,12 @@
 package services
 
 import (
+	"errors"
 	"simple-social-media-API/features/user"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type userUseCase struct {
@@ -19,17 +22,30 @@ func New(ud user.UserData) user.UserService {
 }
 
 func (uuc *userUseCase) Register(newUser user.Core) (user.Core, error) {
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+	newUser.Password = string(hashed)
+	res, err := uuc.qry.Register(newUser)
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "duplicated") {
+			msg = "user already exist"
+		} else {
+			msg = "server problem"
+		}
+		return user.Core{}, errors.New(msg)
+	}
 
+	return res, nil
 }
 func (uuc *userUseCase) Login(email, password string) (string, user.Core, error) {
-
+	return "", user.Core{}, nil
 }
 func (uuc *userUseCase) Profile(token interface{}) (user.Core, error) {
-
+	return user.Core{}, nil
 }
 func (uuc *userUseCase) Update(token interface{}, updatedData user.Core) (user.Core, error) {
-
+	return user.Core{}, nil
 }
 func (uuc *userUseCase) Deactive(token interface{}) error {
-
+	return nil
 }
