639e4d7a167b62a10f0ebd8d02557df3aa1a5fb3
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-15 23:21:46 +0700

    feat: add query for user register

--- features/user/data/query.go
+++ features/user/data/query.go
@@ -17,17 +17,25 @@ func New(db *gorm.DB) user.UserData {
 }
 
 func (uq *userQuery) Register(newUser user.Core) (user.Core, error) {
+	cnv := CoreToData(newUser)
+	err := uq.db.Create(&cnv).Error
+	if err != nil {
+		return user.Core{}, err
+	}
 
+	newUser.ID = cnv.ID
+
+	return newUser, nil
 }
 func (uq *userQuery) Login(email string) (user.Core, error) {
-
+	return user.Core{}, nil
 }
 func (uq *userQuery) Profile(id uint) (user.Core, error) {
-
+	return user.Core{}, nil
 }
 func (uq *userQuery) Update(id uint, updatedData user.Core) (user.Core, error) {
-
+	return user.Core{}, nil
 }
 func (uq *userQuery) Deactive(id uint) error {
-
+	return nil
 }
