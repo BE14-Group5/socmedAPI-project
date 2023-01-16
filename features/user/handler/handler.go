f83eb2583744b64f8a92944497dbef3475aa6377
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-16 08:38:45 +0700

    fix: error handling

--- features/user/handler/handler.go
+++ features/user/handler/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"simple-social-media-API/features/user"
 	"simple-social-media-API/helper"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
@@ -24,37 +25,42 @@ func (uc *userControl) Register() echo.HandlerFunc {
 		input := RegisterRequest{}
 		if err := c.Bind(&input); err != nil {
 			log.Println("error bind input")
-			return c.JSON(http.StatusBadRequest, "wrong input")
+			return c.JSON(http.StatusBadRequest, helper.ErrorResponse("wrong input"))
 		}
 
 		if file, err := c.FormFile("profile_photo"); err != nil {
 			log.Println("error read profile_photo")
-			return c.JSON(http.StatusBadRequest, "wrong image input")
+			return c.JSON(http.StatusBadRequest, helper.ErrorResponse("wrong image input"))
 		} else {
 			dir, err := helper.UploadProfilePhoto(*file, input.Email)
 			if err != nil {
 				log.Println("error running UploadProfilePhoto")
-				return c.JSON(http.StatusInternalServerError, "server problem")
+				return c.JSON(http.StatusInternalServerError, helper.ErrorResponse("server problem"))
 			}
 			input.ProfilePhoto = dir
 		}
 
 		if file, err := c.FormFile("background_photo"); err != nil {
 			log.Println("error read background_photo ")
-			return c.JSON(http.StatusBadRequest, "wrong image input")
+			return c.JSON(http.StatusBadRequest, helper.ErrorResponse("wrong image input"))
 		} else {
 			dir, err := helper.UploadBackgroundPhoto(*file, input.Email)
 			if err != nil {
 				log.Println("error running UploadBackgroundPhoto")
-				return c.JSON(http.StatusInternalServerError, "server problem")
+				return c.JSON(http.StatusInternalServerError, helper.ErrorResponse("server problem"))
 			}
 			input.BackgroundPhoto = dir
 		}
 
 		res, err := uc.srv.Register(*ToCore(input))
 		if err != nil {
-			log.Println("error running register service")
-			return c.JSON(http.StatusInternalServerError, "server problem")
+			if strings.Contains(err.Error(), "exist") {
+				log.Println("error running register service: user already exist")
+				return c.JSON(http.StatusConflict, helper.ErrorResponse("user or email already exist"))
+			} else {
+				log.Println("error running register service")
+				return c.JSON(http.StatusInternalServerError, helper.ErrorResponse("server problem"))
+			}
 		}
 		return c.JSON(http.StatusCreated, map[string]interface{}{
 			"data":    res,
