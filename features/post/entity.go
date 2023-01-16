0fd87d7bc1a278059da53f26f2f392c7df95c8bc
Eka Cahya Putra <ekacahyaps@gmail.com>
2023-01-17 00:39:20 +0700

    chore: add entity.go

---
+++ features/post/entity.go
@@ -0,0 +1,35 @@
+package post
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	ID     uint
+	Body   string
+	Image  string
+	UserID uint
+	Writer string
+}
+
+type PostHandler interface {
+	Add() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+	MyPosts() echo.HandlerFunc
+	AllPosts() echo.HandlerFunc
+}
+
+type PostService interface {
+	Add(token interface{}, newPost Core) (Core, error)
+	Update(token interface{}, postID uint, updatedPost Core) (Core, error)
+	Delete(token interface{}, postID uint) error
+	MyPosts(token interface{}) ([]Core, error)
+	AllPosts() ([]Core, error)
+}
+
+type PostData interface {
+	Add(userID uint, newPost Core) (Core, error)
+	Update(postID uint, userID uint, updatedPost Core) (Core, error)
+	Delete(postID uint, userID uint) error
+	MyPosts(userID uint) ([]Core, error)
+	AllPosts() ([]Core, error)
+}
