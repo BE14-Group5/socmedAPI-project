dac1b43b13018e8cfbb83795a811104edd94e5f3
Eka Cahya Putra <ekacahyaps@gmail.com>
2023-01-17 00:58:13 +0700

    feat: add handler for add post

---
+++ features/post/handler/handler.go
@@ -0,0 +1,70 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+	"simple-social-media-API/features/post"
+	"simple-social-media-API/helper"
+
+	"github.com/labstack/echo/v4"
+)
+
+type postHandle struct {
+	srvc post.PostService
+}
+
+func Isolation(ps post.PostService) post.PostHandler {
+	return &postHandle{
+		srvc: ps,
+	}
+}
+
+func (ph *postHandle) Add() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := c.Get("user")
+		input := AddPostRequest{}
+		if err := c.Bind(&input); err != nil {
+			log.Println("add post body scan error")
+			return c.JSON(http.StatusBadRequest, "format inputan salah")
+		}
+
+		if file, err := c.FormFile("image"); err != nil {
+			log.Println("error read image")
+			return c.JSON(http.StatusBadRequest, helper.ErrorResponse("wrong image input"))
+		} else {
+			dir, err := helper.UploadPostPhotoS3(*file, helper.ExtractToken(token))
+			if err != nil {
+				log.Println("error running UploadPostImage")
+				return c.JSON(http.StatusInternalServerError, helper.ErrorResponse("server problem"))
+			}
+			input.Image = dir
+		}
+
+		res, err := ph.srvc.Add(token, *ToCore(input))
+		if err != nil {
+			log.Println("error running add post service")
+			return c.JSON(http.StatusInternalServerError, helper.ErrorResponse("server problem"))
+		}
+
+		return c.JSON(http.StatusCreated, map[string]interface{}{
+			"data":    AddPostToResponse(res),
+			"message": "success posting",
+		})
+	}
+}
+
+func (ph *postHandle) Update() echo.HandlerFunc {
+	return nil
+}
+
+func (ph *postHandle) Delete() echo.HandlerFunc {
+	return nil
+}
+
+func (ph *postHandle) MyPosts() echo.HandlerFunc {
+	return nil
+}
+
+func (ph *postHandle) AllPosts() echo.HandlerFunc {
+	return nil
+}
