3102d01528f4e253774b7a7d9581906e3076c4cf
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-17 14:05:00 +0700

    feat: add http request for profile

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	uh "simple-social-media-API/features/user/handler"
 	us "simple-social-media-API/features/user/services"
 
+	echojwt "github.com/labstack/echo-jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -29,6 +30,8 @@ func main() {
 	e.Static("/files", "./files")
 	e.POST("/register", userHdl.Register())
 	e.POST("/login", userHdl.Login())
+
+	e.GET("/users", userHdl.Profile(), echojwt.JWT([]byte(config.JWT_KEY)))
 
 	if err := e.Start(":8000"); err != nil {
 		log.Println(err.Error())
