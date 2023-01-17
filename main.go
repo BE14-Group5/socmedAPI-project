b8c2fc0ad177b9702666f6adc0f7a15b93532aec
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-17 14:11:53 +0700

    fix: use old JWT middleware

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	uh "simple-social-media-API/features/user/handler"
 	us "simple-social-media-API/features/user/services"
 
-	echojwt "github.com/labstack/echo-jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -31,7 +30,7 @@ func main() {
 	e.POST("/register", userHdl.Register())
 	e.POST("/login", userHdl.Login())
 
-	e.GET("/users", userHdl.Profile(), echojwt.JWT([]byte(config.JWT_KEY)))
+	e.GET("/users", userHdl.Profile(), middleware.JWT([]byte(config.JWT_KEY)))
 
 	if err := e.Start(":8000"); err != nil {
 		log.Println(err.Error())
