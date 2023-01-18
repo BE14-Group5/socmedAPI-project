456f8107853e82c3a4bc4ee4ebfbda4fcfb323b2
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-18 10:13:42 +0700

    chore: add struct comment, core, model, request

--- features/comment/data/model.go
+++ features/comment/data/model.go
@@ -1,1 +1,36 @@
 package data
+
+import (
+	"simple-social-media-API/features/comment"
+
+	"gorm.io/gorm"
+)
+
+type Comment struct {
+	gorm.Model
+	UserId  uint
+	PostId  uint
+	Content string
+}
+
+func ToCore(data Comment) comment.Core {
+	return comment.Core{
+		ID:        data.ID,
+		CreatedAt: data.CreatedAt,
+		UserId:    data.UserId,
+		PostId:    data.PostId,
+		Content:   data.Content,
+	}
+}
+
+func CoreToData(data comment.Core) Comment {
+	return Comment{
+		Model: gorm.Model{
+			ID:        data.ID,
+			CreatedAt: data.CreatedAt,
+		},
+		UserId:  data.UserId,
+		PostId:  data.PostId,
+		Content: data.Content,
+	}
+}
--- features/comment/entity.go
+++ features/comment/entity.go
@@ -1,1 +1,36 @@
 package comment
+
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Core struct {
+	ID        uint      `json:"id" form:"id"`
+	UserId    uint      `json:"user_id" form:"user_id"`
+	PostId    uint      `json:"post_id" form:"post_id"`
+	CreatedAt time.Time `json:"created_at" form:"created_at"`
+	Content   string    `json:"content" form:"content"`
+}
+
+type CommentHandler interface {
+	Add() echo.HandlerFunc
+	GetComments() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+}
+
+type CommentService interface {
+	Add(token interface{}, newComment Core, postId uint) (Core, error)
+	GetComments() ([]Core, error)
+	Update(token interface{}, updComment Core, postId, commentId uint) (Core, error)
+	Delete(token interface{}, postId, commentId uint) error
+}
+
+type CommentData interface {
+	Add(newComment Core) (Core, error)
+	GetComments() ([]Core, error)
+	Update(updComment Core) (Core, error)
+	Delete(userId, postId, commentId uint) error
+}
