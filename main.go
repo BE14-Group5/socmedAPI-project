6a9cf8dd3337289903caff5ad9c0ca74c1a7f1e8
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-18 18:57:28 +0700

    feat: add http request

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 	e.GET("/posts/:id", postHdl.GetPostById(), middleware.JWT([]byte(config.JWT_KEY)))
 	e.GET("/allposts", postHdl.AllPosts())
 
+	e.GET("/comments", commentHdl.GetComments())
 	e.POST("/comments", commentHdl.Add(), middleware.JWT([]byte(config.JWT_KEY)))
 
 	if err := e.Start(":8000"); err != nil {
