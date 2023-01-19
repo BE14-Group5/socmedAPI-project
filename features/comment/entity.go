d003af9d83373f63dc8edd4ed03a15745c4da960
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-19 15:48:57 +0700

    fix: update parameter update comment

--- features/comment/entity.go
+++ features/comment/entity.go
@@ -23,12 +23,12 @@ type CommentHandler interface {
 
 type CommentService interface {
 	Add(token interface{}, newComment Core) (Core, error)
-	Update(token interface{}, updComment Core, postId, commentId uint) (Core, error)
+	Update(token interface{}, commentId uint, postId uint, updComment Core) (Core, error)
 	Delete(token interface{}, postId, commentId uint) error
 }
 
 type CommentData interface {
 	Add(newComment Core) (Core, error)
-	Update(updComment Core) (Core, error)
+	Update(userId uint, commentId uint, postId uint, updComment Core) (Core, error)
 	Delete(userId, postId, commentId uint) error
 }
